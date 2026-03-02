63d7d3bc682caeda7206b0ede190b70bb881983d
Tejas Patel <patel.s.tejas@gmail.com>
2026-03-01 18:27:30 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const audio = document.getElementById("radio");
+const button = document.getElementById("playBtn");
+
+button.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();
+        button.textContent = "STOP THE NOISE";
+    }else {
+        audio.pause();
+        button.textContent = "ENTER THE PIT";
+    }
+});
+
+async function fetchMetadata() {
+    try {
+        const response = await fetch("https://relevantly-nooklike-xavi.ngrok-free.dev/status-json.xsl");
+        const data = await response.json();
+
+        const source = data.icestats.source;
+
+        document.getElementById("song").textContent = source.title || "Unknown Track";
+    } catch (error) {
+        document.getElementById("song").textContent = "Stream Offline";
+    }
+}
+
+setInterval(fetchMetadata, 5000);
+fetchMetadata();
